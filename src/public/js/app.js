ada5920f23daa4c13970c7ec6042b94126aa2670
doosicEE <strou73@naver.com>
2022-09-26 11:55:11 +0900

    Add #1 Chat Completed
    서로 다른 브라우저끼리 접속하여 채팅을 입력했을 때에 서로에게 메세지가 전달되도록 만들어보았다.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,5 @@
+const messageList = document.querySelector("ul")
+const messageForm = document.querySelector("form")
 const socket = new WebSocket(`ws://${window.location.host}`);
 
 socket.addEventListener("open", () => {
@@ -5,13 +7,36 @@ socket.addEventListener("open", () => {
 })
 
 socket.addEventListener("message", (message) => {
-    console.log("New message: ", message.data);
+    if (message.data instanceof Blob){
+        reader = new FileReader();
+
+        // 파일(메세지)를 읽어들인다.
+        reader.readAsText(message.data);
+
+        // 읽는 동작이 성공적으로 완료되었을때 발생
+        reader.onload = () => {
+            console.log(reader.result);
+        };
+
+    }else {
+       console.log(message.data);
+    }
+
 })
 
 socket.addEventListener("close", () => {
     console.log("Disconnected from Server");
 })
 
-setTimeout(() => {
-    socket.send("hello from the browser!")
-}, 10000);
+// setTimeout(() => {
+//     socket.send("hello from the browser!")
+// }, 10000);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
