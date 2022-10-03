802b42877a71cdbd428ee73bd7e9a853988259a8
DoosicEE <strou73@naver.com>
2022-10-03 23:11:39 +0900

    Add #5 SocketIO is Amazing
    Server에서 보낸 function을 front에서 호출하여 사용 할 수 있다. 즉, server에 작성된 코드를 front에서 호출할 수 있다. 이 기능은 뭔가 새로운 가능성을 보여준 것 같다.  무얼 할 수 있을지는 당장은 떠오르지 않으나 다음 강의들에서 보여줄것 같고 더 나아가서는 내가 응용 가능할 것이다.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -11,3 +11,17 @@
     var parsed = url(uri, opts.path || "/socket.io");
 */
 const socket = io();
+
+const welcome = document.getElementById("welcome")
+const form = welcome.querySelector("form")
+
+function handleRoomSubmit(event){
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room", { payload: input.value }, () => {
+        console.log("server is done!");
+    })
+    input.value = "";
+}
+
+form.addEventListener("submit", handleRoomSubmit);
