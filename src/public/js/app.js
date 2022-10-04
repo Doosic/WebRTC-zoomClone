efa4a6d7c8b0e6f48a72fb2784d38b36523c1fb7
DoosicEE <strou73@naver.com>
2022-10-04 23:42:00 +0900

    Add #5 Recap
    front에서 마지막 인자값에 function을 담아 server로 보내줄 수 있고, server에서는 function을 실행시켜 줄 수있다. 참고로 인자값도 넣어서 실행 시켜줄 수 있다!

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -15,12 +15,18 @@ const socket = io();
 const welcome = document.getElementById("welcome")
 const form = welcome.querySelector("form")
 
+function backendDone(){
+    console.log("backendDone");
+}
+
 function handleRoomSubmit(event){
     event.preventDefault();
     const input = form.querySelector("input");
-    socket.emit("enter_room", { payload: input.value }, () => {
-        console.log("server is done!");
-    })
+    socket.emit(
+        "enter_room",
+        input.value,
+        backendDone()
+        )
     input.value = "";
 }
 
