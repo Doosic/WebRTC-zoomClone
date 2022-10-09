ea1f31a03377c80e0d2b370e9acb98e2ff68e79d
DoosicEE <strou73@naver.com>
2022-10-09 22:38:22 +0900

    Add #5 NickNames
    front에서 server로 function을 보내줄때에 이슈.
    front에서 input.value를 보낸 후 input.value를 바로 비워줬다. 그런데 server에서 해당 function을 실행시키는 시점이 input.value를 비우는 시점보다 늦으므로 "" 즉, 빈 공백값이 들어가 있는 문제가 있다. 다른 변수에 담아서 화면에 띄워주고 input.value를 초기화 하자.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -27,11 +27,29 @@ function addMessage(message){
     ul.appendChild(li);
 }
 
+function handleMessageSubmit(event){
+    event.preventDefault()
+    const input = room.querySelector("input");
+    /*
+        input.value를 변수에 한번 더 담는 이유는
+        그냥 input.value를 addMessage로 보내고 비워줬을 때에는
+        함수가 실행되기전 input.value = "";가 먼저 실행되어
+        함수가 실행될때에는 "" 값을 가지고 실행되기 때문이다.
+    */
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value = "";
+}
+
 function showRoom(){
     welcome.hidden = true;
     room.hidden = false;
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
+    const form = room.querySelector("form")
+    form.addEventListener("submit", handleMessageSubmit);
 }
 
 function handleRoomSubmit(event){
@@ -48,3 +66,9 @@ form.addEventListener("submit", handleRoomSubmit);
 socket.on("welcome", () => {
     addMessage("someone joined!");
 })
+
+socket.on("bye", () => {
+    addMessage("someone left");
+})
+
+socket.on("new_message", addMessage);
